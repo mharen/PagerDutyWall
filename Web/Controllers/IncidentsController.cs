d150a12204e2a68a35b27ba808165fd8beac4636
Michael Haren <mharen@gmail.com>
2014-04-10 22:01:04 -0400

    initial commit (fake data!)

---
+++ Web/Controllers/IncidentsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class IncidentsController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<Row> Get()
+        {
+            return new[] {
+                new Row{
+                    sDate = "today",                       
+                    sTime = "13:30",                       
+                    sDeparture = "Rabbit Memory Alert CRITICAL",        
+                    nStatus = 1,                           
+                    nTrack = "JP",                           
+                    bLight = true                          
+                },                                       
+                new Row{
+                    sDate = "yesterday",                   
+                    sTime = "13:32",                       
+                    sDeparture = "Rabbit Sockets Alert CRITICAL",        
+                    nStatus = 2,                           
+                    nTrack = "DS",                           
+                    bLight = false                          
+                }                                 
+            };
+        }
+    }
+}
---
+++ Web/Models/Row.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class Row
+    {
+        public string sDate { get; set; }
+        public string sTime { get; set; }
+        public string sDeparture { get; set; }
+        public int nStatus { get; set; }
+        public string nTrack { get; set; }
+        public bool bLight { get; set; }
+    }
+}
